653014b52f9f92704f224c296e381a972d23cd0e
xu_mj <331538695@qq.com>
2023-12-15 11:33:53 +0800

    fix: fix tracing print and remove unused code

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-mod service;
 mod model;
+mod service;
 
-use axum::{
-    routing::get,
-    Router,
-};
-use tokio::sync::mpsc;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use crate::model::manager::Manager;
 use crate::service::ws::websocket_handler;
-
-
-
+use axum::{routing::get, Router};
+use tokio::sync::mpsc;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "example_chat=trace".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
+    tracing_subscriber::fmt()
+        .with_max_level(Level::DEBUG)
         .init();
-
     let (tx, rx) = mpsc::channel(1024);
     let hub = Manager::new(tx);
     let mut cloned_hub = hub.clone();
@@ -34,9 +23,7 @@ async fn main() {
         .route("/ws/:id", get(websocket_handler))
         .with_state(hub);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
