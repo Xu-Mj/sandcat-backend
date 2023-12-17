1c11c97eed208c32042b6dfe4d3a6084cf420acf
xu_mj <331538695@qq.com>
2023-12-17 09:25:05 +0800

    feat: 提取路由到独立文件

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
-mod model;
+mod domain;
+mod errors;
+mod handlers;
+mod infra;
+mod routes;
 mod service;
+mod utils;
 
-use crate::model::manager::Manager;
-use crate::service::ws::websocket_handler;
-use axum::{routing::get, Router};
+use crate::errors::internal_error;
+use crate::routes::app_routes;
+use deadpool_diesel::postgres::{Manager, Pool};
+use domain::model::manager;
+use dotenvy::dotenv;
+use std::env;
+use std::net::SocketAddr;
 use tokio::sync::mpsc;
 use tracing::Level;
+use crate::handlers::users::UserRequest;
 
+#[derive(Clone)]
+pub struct AppState {
+    pool: Pool,
+    hub: manager::Manager,
+}
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(Level::DEBUG)
         .init();
     let (tx, rx) = mpsc::channel(1024);
-    let hub = Manager::new(tx);
+    let hub = manager::Manager::new(tx);
     let mut cloned_hub = hub.clone();
     tokio::spawn(async move {
         cloned_hub.run(rx).await;
     });
+    dotenv().expect("need .env file");
+    // create pool
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // Create a connection pool to the PostgreSQL database
+    let manager = Manager::new(database_url, deadpool_diesel::Runtime::Tokio1);
+    let pool = Pool::builder(manager).build().unwrap();
+    let app_state = AppState { pool, hub };
+    let app = app_routes(app_state.clone()).with_state(app_state);
 
-    let app = Router::new()
-        .route("/ws/:id", get(websocket_handler))
-        .with_state(hub);
+    // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let address = "0.0.0.0:3000".to_string();
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    let socket_addr: SocketAddr = address.parse().unwrap();
+    tracing::debug!("listening on {}", address);
+    axum::Server::bind(&socket_addr)
+        .serve(app.into_make_service())
+        .await
+        .map_err(internal_error)
+        .unwrap()
 }
---
+++ src/routes.rs
@@ -0,0 +1,24 @@
+use crate::handlers::users::{create_user, get_user_by_id};
+use crate::service::ws::websocket_handler;
+use crate::AppState;
+use axum::routing::{get, post};
+use axum::Router;
+
+pub(crate) fn app_routes(state: AppState) -> Router<AppState> {
+    Router::new()
+        .nest("/user", user_routes(state.clone()))
+        .nest("/ws", ws_routes(state.clone()))
+}
+
+fn user_routes(state: AppState) -> Router<AppState> {
+    Router::new()
+        .route("/", post(create_user))
+        .route("/:id", get(get_user_by_id))
+        .with_state(state)
+}
+
+fn ws_routes(state: AppState) -> Router<AppState> {
+    Router::new()
+        .route("/:id", get(websocket_handler))
+        .with_state(state)
+}
