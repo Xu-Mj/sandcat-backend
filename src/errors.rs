17e2a5036a8efc519712a66c88d3d77cbb3d93ad
xu_mj <331538695@qq.com>
2023-12-17 10:15:24 +0800

    fix: 修改全局错误处理，提供更多的错误信息

--- src/errors.rs
+++ src/errors.rs
@@ -4,30 +4,41 @@ use axum::Json;
 use serde_json::json;
 use std::error::Error;
 
+type Msg = String;
+type Location = String;
+type Path = String;
 /// 全局变量
 #[derive(Debug)]
 pub enum AppError {
     // 500
-    InternalServerError(String),
+    InternalServer(String),
     // 400
-    BodyParsingError(String),
+    BodyParsing(Msg, Path),
+    PathParsing(Msg, Option<Location>),
 }
 
 pub fn internal_error<E: Error>(err: E) -> AppError {
-    AppError::InternalServerError(err.to_string())
+    AppError::InternalServer(err.to_string())
 }
 // 实现axum的into response特征，将自定义错误转为axum的响应
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, msg) = match self {
-            AppError::InternalServerError(msg) => (
+            AppError::InternalServer(msg) => (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 format!("INTERNAL SERVER ERROR: {}", msg),
             ),
-            AppError::BodyParsingError(msg) => (
+            AppError::BodyParsing(msg, path) => (
                 StatusCode::BAD_REQUEST,
-                format!("Bad Request error: {}", msg),
+                format!("Bad Request error: {{ message: {}, path: {}}}", msg, path),
             ),
+            AppError::PathParsing(msg, location) => {
+                let mut msg = format!("Bad Request error: {}", msg);
+                if location.is_some() {
+                    msg = format!("Bad Request error: in {} : {}", location.unwrap(), msg);
+                }
+                (StatusCode::BAD_REQUEST, msg)
+            }
         };
         (status, Json(json!({"message":msg}))).into_response()
     }
