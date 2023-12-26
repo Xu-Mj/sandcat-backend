84285f9e4ab6dd3faca8a45fb3bb759bea7b15a4
xu_mj <331538695@qq.com>
2023-12-26 11:15:26 +0800

    feat: 新增friends模块代码

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,1 +1,3 @@
-pub(crate) mod  users;
+pub(crate) mod friends;
+pub(crate) mod users;
+pub(crate) mod ws;
--- src/infra/repositories/mod.rs
+++ src/infra/repositories/mod.rs
@@ -1,1 +1,4 @@
 pub(crate) mod user_repo;
+pub(crate) mod friendship_repo;
+pub(crate) mod friends;
+pub(crate) mod  messages;
