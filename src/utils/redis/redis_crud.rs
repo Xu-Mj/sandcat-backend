20e91eaa6e3c3a0b76d6a7f6fe883850f52ba499
xu_mj <331538695@qq.com>
2023-12-26 11:09:53 +0800

    feat: 新增redis模块

---
+++ src/utils/redis/redis_crud.rs
@@ -0,0 +1,29 @@
+use crate::infra::errors::InfraError;
+use redis::aio::Connection;
+use redis::AsyncCommands;
+use serde::Serialize;
+
+pub async fn set_string<T: Serialize>(
+    mut conn: Connection,
+    key: String,
+    value: T,
+) -> Result<String, InfraError> {
+    let value = serde_json::to_string(&value)
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    // redis 还提供了json特性，底层也是通过serde_json::to_string来实现的，用不用都行
+
+    let result = conn
+        .set(key, value)
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    Ok(result)
+}
+
+pub async fn del(mut conn: Connection, key: String) -> Result<(), InfraError> {
+      conn
+        .del(key)
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    Ok(())
+}
+
