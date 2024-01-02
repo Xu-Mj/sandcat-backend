5a68f05e4c7ec889b106b0757e31adebdbd0cc1f
xu_mj <331538695@qq.com>
2024-01-02 19:25:02 +0800

    feat: 修改客户端存储方式为map，提高查找效率

--- src/domain/model/mod.rs
+++ src/domain/model/mod.rs
@@ -9,5 +9,11 @@ pub(crate) mod manager;
 pub(crate) mod msg;
 pub(crate) mod user;
 
-type ClientSender = SplitSink<WebSocket, Message>;
-type Hub = Arc<RwLock<HashMap<i32, ClientSender>>>;
+type ClientSender = Arc<RwLock< SplitSink<WebSocket, Message>>>;
+// 修改客户端为数组，包含桌面端以及移动端。。。Hub的key就是用户id，这样在发送消息时就不需要遍历查找了，提高一点性能
+pub struct Client {
+    pub id: String,
+    pub sender: ClientSender,
+}
+
+type Hub = Arc<RwLock<HashMap<String, HashMap<String,Client>>>>;
