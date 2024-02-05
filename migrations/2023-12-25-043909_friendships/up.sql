0159c7c7c16f196d6d6a77bb0c9375cec2a93856
xu_mj <331538695@qq.com>
2024-02-05 21:03:25 +0800

    feat: 增加好友关系表

--- migrations/2023-12-25-043909_friendships/up.sql
+++ migrations/2023-12-25-043909_friendships/up.sql
@@ -1,14 +1,17 @@
 create table friendships
 (
-    id          varchar primary key,
-    user_id     varchar   not null,
-    friend_id   varchar   not null,
-    status      char      not null default '0',
-    apply_msg   varchar,
-    source        varchar,
-    create_time timestamp not null default now(),
-    update_time timestamp not null default now(),
+    id           varchar primary key,
+    user_id      varchar   not null,
+    friend_id    varchar   not null,
+    status       char      not null default '0',
+    apply_msg    varchar,
+    source       varchar,
+    is_delivered bool      not null default false,
+    create_time  timestamp not null default now(),
+    update_time  timestamp not null default now(),
     FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (friend_id) REFERENCES users (id)
-)-- Your SQL goes here
+    FOREIGN KEY (friend_id) REFERENCES users (id),
+    Unique (user_id, friend_id)
+)
+-- Your SQL goes here
 -- Your SQL goes here
