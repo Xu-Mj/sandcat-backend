658842aa53d79fba73ca31c9e3fc964b9fc7cb75
xu_mj <331538695@qq.com>
2024-02-05 21:01:58 +0800

    feat: 增加用户相关的多个接口

--- src/handlers/users/user_handlers.rs
+++ src/handlers/users/user_handlers.rs
@@ -1,44 +1,226 @@
-
+use crate::config::CONFIG;
+use crate::domain::model::user::{User, UserError};
+use crate::handlers::users::UserRegister;
+use crate::infra::errors::InfraError;
+use crate::infra::repositories::user_repo::{get, insert, search, verify_pwd, NewUserDb};
+use crate::service::ws::ws_service::register_ws;
+use crate::utils::redis::redis_crud;
+use crate::utils::{JsonExtractor, JsonWithAuthExtractor, PathExtractor};
+use crate::AppState;
 use axum::extract::State;
 use axum::Json;
+use jsonwebtoken::{encode, EncodingKey, Header};
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
 use nanoid::nanoid;
-use crate::domain::model::user::{User, UserError};
-use crate::infra::repositories::user_repo::{get, insert, NewUserDb};
-use crate::utils::{JsonExtractor, PathExtractor};
-use crate::AppState;
-use crate::handlers::users::UserRequest;
-use crate::infra::errors::InfraError;
+use rand::Rng;
+use redis::{AsyncCommands, Commands};
+use serde::{Deserialize, Serialize};
 
 pub async fn create_user(
     State(app_state): State<AppState>,
-    JsonExtractor(new_user): JsonExtractor<UserRequest>,
+    JsonExtractor(new_user): JsonExtractor<UserRegister>,
 ) -> Result<Json<User>, UserError> {
+    let mut redis_conn = app_state
+        .redis
+        .get_async_connection()
+        .await
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+
+    // 查询验证码
+
+    redis_conn
+        .get(&new_user.email)
+        .await
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
     // 结构体转换
     let user2db = NewUserDb {
+        id: nanoid!(),
         name: new_user.name,
         account: nanoid!(),
+        password: new_user.password,
+        email: Some(new_user.email),
         avatar: new_user.avatar,
-        gender: new_user.gender,
-        phone: new_user.phone,
-        email: new_user.email,
-        address: new_user.address,
-        birthday: new_user.birthday,
+        ..Default::default()
     };
     // repo方法调用
-    let user = insert(&app_state.pool, user2db).await.map_err(UserError::InfraError)?;
+    let user = insert(&app_state.pool, user2db)
+        .await
+        .map_err(UserError::InfraError)?;
     // 结果返回
     Ok(Json(user))
 }
 
 pub async fn get_user_by_id(
     State(app_state): State<AppState>,
-    PathExtractor(id): PathExtractor<i32>,
+    PathExtractor(id): PathExtractor<String>,
 ) -> Result<Json<User>, UserError> {
-    let user = get(&app_state.pool, id).await.map_err(|err| {
-        match err {
+    let user = get(&app_state.pool, id.clone())
+        .await
+        .map_err(|err| match err {
             InfraError::InternalServerError(e) => UserError::InternalServerError(e),
-            InfraError::NotFound => UserError::NotFound(id)
-        }
-    })?;
+            InfraError::NotFound => UserError::NotFound(id),
+        })?;
     Ok(Json(user))
+}
+pub async fn search_user(
+    State(app_state): State<AppState>,
+    PathExtractor(pattern): PathExtractor<String>,
+) -> Result<Json<Vec<User>>, UserError> {
+    let users = search(&app_state.pool, pattern.clone())
+        .await
+        .unwrap_or_else(|_| vec![]);
+    Ok(Json(users))
+}
+
+pub async fn logout(
+    State(app_state): State<AppState>,
+    PathExtractor(uuid): PathExtractor<String>,
+) -> Result<(), UserError> {
+    let conn = app_state
+        .redis
+        .get_async_connection()
+        .await
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+    redis_crud::del(conn, uuid)
+        .await
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+    tracing::debug!("user logout");
+    Ok(())
+}
+pub async fn test_auth(
+    State(app_state): State<AppState>,
+    JsonWithAuthExtractor(test): JsonWithAuthExtractor<LoginRequest>,
+) -> Result<(), UserError> {
+    tracing::debug!("Test: {:?}", test);
+    Ok(())
+}
+#[derive(Serialize)]
+pub struct Token {
+    id: String,
+    user: User,
+    token: String,
+    ws_addr: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LoginRequest {
+    pub account: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Claims {
+    pub sub: String,
+    pub exp: u64,
+    pub iat: u64,
+    pub update: u64,
+}
+
+impl Claims {
+    pub fn new(sub: String) -> Self {
+        let now = chrono::Local::now().timestamp_millis() as u64;
+        let exp = now + 10_000;
+        Self {
+            sub,
+            exp,
+            iat: now,
+            update: 10_000,
+        }
+    }
+}
+pub async fn login(
+    State(app_state): State<AppState>,
+    JsonExtractor(login): JsonExtractor<LoginRequest>,
+) -> Result<Json<Token>, UserError> {
+    let user = verify_pwd(&app_state.pool, login.account, login.password)
+        .await
+        .map_err(|err| {
+            tracing::error!("login error: {:?}", &err);
+            match err {
+                InfraError::InternalServerError(e) => UserError::InternalServerError(e),
+                InfraError::NotFound => UserError::LoginError,
+            }
+        })?;
+    // 生成token
+    let claims = Claims::new(user.name.clone());
+
+    let token = encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(CONFIG.get().unwrap().jwt_secret().as_bytes()),
+    )
+    .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+    let ws_addr = register_ws(app_state.redis, user.id.clone())
+        .await
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+    Ok(Json(Token {
+        id: user.account.clone(),
+        user,
+        token,
+        ws_addr,
+    }))
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct Email {
+    pub email: String,
+}
+pub async fn send_email(
+    State(state): State<AppState>,
+    JsonExtractor(email): JsonExtractor<Email>,
+) -> Result<(), UserError> {
+    if email.email.is_empty() {
+        return Err(UserError::InternalServerError(
+            "parameter is none".to_string(),
+        ));
+    }
+    let mut redis_conn = state
+        .redis
+        .get_async_connection()
+        .await
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+    // 生成随机数（验证码）
+    let mut rng = rand::thread_rng();
+    let num: u32 = rng.gen_range(100_000..1_000_000);
+    // 将随机数存入redis--五分钟有效
+    let e = email.email.clone();
+    tokio::spawn(async move {
+        let _: () = redis_conn
+            .set_ex(e, num, 60 * 5)
+            .await
+            .map_err(|err| UserError::InternalServerError(err.to_string()))
+            .unwrap();
+    });
+
+    tracing::debug!("验证码：{:?}, 邮箱: {:?}", num, &email.email);
+    let email = Message::builder()
+        .from(
+            "653609824@qq.com"
+                .parse()
+                .map_err(|_| UserError::InternalServerError("邮箱parse失败".to_string()))?,
+        )
+        .to(email
+            .email
+            .parse()
+            .map_err(|_| UserError::InternalServerError("邮箱parse失败".to_string()))?)
+        .subject("验证登录")
+        .header(ContentType::TEXT_PLAIN)
+        .body(num.to_string())
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?;
+
+    let creds = Credentials::new("653609824@qq.com".to_owned(), "rxkhmcpjgigsbegi".to_owned());
+
+    // Open a remote connection to gmail
+    let mailer = SmtpTransport::relay("smtp.qq.com")
+        .map_err(|err| UserError::InternalServerError(err.to_string()))?
+        .credentials(creds)
+        .build();
+
+    // Send the email
+    mailer
+        .send(&email)
+        .map_err(|e| UserError::InternalServerError(e.to_string()))?;
+    Ok(())
 }
--- src/infra/repositories/user_repo.rs
+++ src/infra/repositories/user_repo.rs
@@ -1,41 +1,43 @@
-use crate::domain::model::user::User;
+use crate::domain::model::user::{User, UserError};
 use crate::infra::db::schema::users;
 use crate::infra::errors::{adapt_infra_error, InfraError};
 use deadpool_diesel::postgres::Pool;
-use diesel::{
-    ExpressionMethods, Insertable, QueryDsl, Queryable, RunQueryDsl, Selectable, SelectableHelper,
-};
+use diesel::{BoolExpressionMethods, ExpressionMethods, Insertable, QueryDsl, Queryable, RunQueryDsl, Selectable, SelectableHelper, TextExpressionMethods};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Queryable, Selectable, Debug)]
-// 指定表明
-#[diesel(table_name=users)]
-// 开启编译期字段检查，主要检查字段类型、数量是否匹配，可选
-#[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct UserDb {
-    pub id: i32,
-    pub name: String,
-    pub account: String,
-    pub avatar: String,
-    pub gender: String,
-    pub phone: Option<String>,
-    pub email: Option<String>,
-    pub address: Option<String>,
-    pub birthday: Option<chrono::NaiveDateTime>,
-    pub create_time: chrono::NaiveDateTime,
-    pub update_time: chrono::NaiveDateTime,
-    pub is_delete: bool,
-}
+// #[derive(Serialize, Queryable, Selectable, Debug)]
+// // 指定表明
+// #[diesel(table_name=users)]
+// // 开启编译期字段检查，主要检查字段类型、数量是否匹配，可选
+// #[diesel(check_for_backend(diesel::pg::Pg))]
+// pub struct UserDb {
+//     pub id: String,
+//     pub name: String,
+//     pub account: String,
+//     pub password: String,
+//     pub avatar: String,
+//     pub gender: String,
+//     pub phone: Option<String>,
+//     pub email: Option<String>,
+//     pub address: Option<String>,
+//     pub birthday: Option<chrono::NaiveDateTime>,
+//     pub create_time: chrono::NaiveDateTime,
+//     pub update_time: chrono::NaiveDateTime,
+//     pub is_delete: bool,
+// }
 
 // 新建用户结构体
-#[derive(Deserialize, Insertable)]
+#[derive(Deserialize, Insertable, Default)]
 // 指定表明
 #[diesel(table_name=users)]
 pub struct NewUserDb {
+    pub id: String,
     pub name: String,
     pub account: String,
+    pub password: String,
     pub avatar: String,
     pub gender: String,
+    pub age: i32,
     pub phone: Option<String>,
     pub email: Option<String>,
     pub address: Option<String>,
@@ -44,6 +46,7 @@ pub struct NewUserDb {
 
 // 新建
 pub async fn insert(pool: &Pool, new_user: NewUserDb) -> Result<User, InfraError> {
+    // 设置user_id
     // 从连接池获取链接, map_err的作用是转换抛出的错误
     let conn = pool.get().await.map_err(adapt_infra_error)?;
     // 执行sql操作
@@ -51,43 +54,66 @@ pub async fn insert(pool: &Pool, new_user: NewUserDb) -> Result<User, InfraError
         .interact(|conn| {
             diesel::insert_into(users::table)
                 .values(new_user)
-                .returning(UserDb::as_returning())
+                .returning(User::as_returning())
                 .get_result(conn)
         })
         .await
         .map_err(adapt_infra_error)?
         .map_err(adapt_infra_error)?;
-    Ok(User::from(result))
+    Ok(result)
+    // Ok(User::from(result))
 }
 
-pub async fn get(pool: &Pool, id: i32) -> Result<User, InfraError> {
+pub async fn get(pool: &Pool, id: String) -> Result<User, InfraError> {
     let conn = pool.get().await.map_err(adapt_infra_error)?;
     let user = conn
         .interact(move |conn| {
             users::table
-                .filter(users::id.eq(id))
-                .select(UserDb::as_select())
+                .filter(users::id.eq(id).and(users::is_delete.eq(false)))
+                .select(User::as_select())
                 .get_result(conn)
         })
         .await
         .map_err(adapt_infra_error)?
         .map_err(adapt_infra_error)?;
-    tracing::debug!("get user by id: {:?}", &user);
-    Ok(convert_user_db2user(user))
+    // tracing::debug!("get user by id: {:?}", &user);
+    Ok(user)
+}
+pub async fn search(pool: &Pool, pattern: String) -> Result<Vec<User>, InfraError> {
+    let conn = pool.get().await.map_err(adapt_infra_error)?;
+    let users = conn
+        .interact(move |conn| {
+            users::table
+                .filter(users::account.eq(pattern.clone()).or(users::name.like(pattern)).and(users::is_delete.eq(false)))
+                .select(User::as_select())
+                .get_results(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    // tracing::debug!("get user by id: {:?}", &user);
+    Ok(users)
 }
 
-pub fn convert_user_db2user(user_db: UserDb) -> User {
-    User {
-        id: user_db.id,
-        name: user_db.name,
-        avatar: user_db.avatar,
-        account: user_db.account,
-        gender: user_db.gender,
-        phone: user_db.phone,
-        email: user_db.email,
-        address: user_db.address,
-        birthday: user_db.birthday,
-        create_time: user_db.create_time,
-        update_time: user_db.update_time,
+pub async fn verify_pwd(pool: &Pool, account: String, pwd: String) -> Result<User, InfraError> {
+    let conn = pool.get().await.map_err(adapt_infra_error)?;
+    let user = conn
+        .interact(move |conn| {
+            users::table
+                .filter(
+                    users::account.eq(account.clone()).or(users::phone
+                        .eq(account.clone())
+                        .or(users::email.eq(account))),
+                )
+                .select(User::as_select())
+                .get_result(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    if user.password == pwd {
+        Ok(user)
+    } else {
+        Err(InfraError::NotFound)
     }
 }
