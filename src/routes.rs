7b66d8d61b1be7e0ac29a06ad7878d12a404290a
xu_mj <331538695@qq.com>
2024-02-05 21:00:56 +0800

    feat: 增加路由

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,10 @@
+use crate::handlers::files::files::{get_file_by_name, upload};
 use crate::handlers::friends::friend_handlers::{
-    agree, black_list, create, deny, get_apply_list_by_user_id, get_friends_list_by_user_id,
+    agree, black_list, create, deny, get_apply_list_by_user_id,
     get_friends_list_by_user_id2, update_friend_remark,
 };
-use crate::handlers::users::user_handlers::logout;
-use crate::handlers::users::{create_user, get_user_by_id, login};
+use crate::handlers::users::user_handlers::{logout, search_user};
+use crate::handlers::users::{create_user, get_user_by_id, login, send_email};
 use crate::handlers::ws::ws_handlers::get_list;
 use crate::service::ws::ws_service::websocket_handler;
 use crate::AppState;
@@ -16,22 +17,25 @@ pub(crate) fn app_routes(state: AppState) -> Router {
         .nest("/friend", friend_routes(state.clone()))
         .nest("/ws", ws_routes(state.clone()))
         .nest("/test", test(state.clone()))
+        .nest("/file", file_routes(state.clone()))
 }
 
 fn user_routes(state: AppState) -> Router {
     Router::new()
         .route("/", post(create_user))
         .route("/:id", get(get_user_by_id))
+        .route("/search/:pattern", get(search_user))
         .route("/login", post(login))
         .route("/logout/:uuid", delete(logout))
+        .route("/mail/send", post(send_email))
         .with_state(state)
 }
 fn friend_routes(state: AppState) -> Router {
     Router::new()
         .route("/", post(create))
         .route("/:id", get(get_friends_list_by_user_id2))
-        .route("/:id/apply/:status", get(get_apply_list_by_user_id))
-        .route("/agree", post(agree))
+        .route("/:id/apply", get(get_apply_list_by_user_id))
+        .route("/agree/:friendship_id", put(agree))
         .route("/blacklist", put(black_list))
         .route("/remark", put(update_friend_remark))
         .route("/:user_id/deny/:friend_id", post(deny))
@@ -40,7 +44,13 @@ fn friend_routes(state: AppState) -> Router {
 
 fn ws_routes(state: AppState) -> Router {
     Router::new()
-        .route("/:id/conn/:token/:user_id", get(websocket_handler))
+        .route("/:user_id/conn/:token/:pointer_id", get(websocket_handler))
+        .with_state(state)
+}
+fn file_routes(state: AppState) -> Router {
+    Router::new()
+        .route("/upload", post(upload))
+        .route("/get/:filename", get(get_file_by_name))
         .with_state(state)
 }
 fn test(state: AppState) -> Router {
