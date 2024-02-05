f396bf770c96503555b841fa8bd00cf28d149d8f
xu_mj <331538695@qq.com>
2024-02-05 21:03:06 +0800

    feat: 修改字段类型

--- migrations/2023-12-15-032329_users/up.sql
+++ migrations/2023-12-15-032329_users/up.sql
@@ -1,11 +1,13 @@
 -- Your SQL goes here
 CREATE TABLE users
 (
-    id          serial PRIMARY KEY,
+    id          varchar PRIMARY KEY,
     name        varchar   not null,
     account     varchar   not null,
+    password    varchar   not null,
     avatar      varchar   not null,
     gender      varchar   not null,
+    age         int       not null default 0,
     phone       varchar(20),
     email       varchar(64),
     address     varchar(1024),
