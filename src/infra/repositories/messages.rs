d447744cfe54d6b23b562fe8534a4b66a3f68d2b
xu_mj <331538695@qq.com>
2024-02-05 20:59:57 +0800

    feat: 增加消息model

---
+++ src/infra/repositories/messages.rs
@@ -0,0 +1,154 @@
+use crate::domain::model::msg::{MessageType, Msg, Single};
+use crate::infra::db::schema::messages;
+use crate::infra::errors::{adapt_infra_error, InfraError};
+use deadpool_diesel::postgres::Pool;
+use diesel::{
+    BoolExpressionMethods, ExpressionMethods, Insertable, QueryDsl, Queryable, RunQueryDsl,
+    Selectable, SelectableHelper,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize, Queryable, Selectable)]
+#[diesel(table_name=messages)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct MsgDb {
+    pub msg_type: String,
+    pub msg_id: String,
+    pub content: String,
+    pub send_id: String,
+    pub friend_id: String,
+    pub content_type: String,
+    pub is_read: bool,
+    pub delivered: bool,
+    pub create_time: chrono::NaiveDateTime,
+}
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize, Queryable, Selectable, Insertable)]
+#[diesel(table_name=messages)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewMsgDb {
+    pub msg_type: String,
+    pub msg_id: String,
+    pub content: String,
+    pub send_id: String,
+    pub friend_id: String,
+    pub content_type: String,
+    pub is_read: bool,
+    pub delivered: bool,
+    pub create_time: chrono::NaiveDateTime,
+}
+
+impl From<Single> for NewMsgDb {
+    fn from(msg: Single) -> Self {
+        Self {
+            msg_type: MessageType::Single.to_string(),
+            msg_id: msg.msg_id,
+            content: msg.content,
+            send_id: msg.send_id,
+            friend_id: msg.friend_id,
+            content_type: msg.content_type.to_string(),
+            is_read: false,
+            delivered: false,
+            create_time: chrono::Local::now().naive_local(),
+        }
+    }
+}
+
+// 插入一条消息
+pub async fn insert_msg(pool: &Pool, new_msg_db: NewMsgDb) -> Result<NewMsgDb, InfraError> {
+    let conn = pool
+        .get()
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    let msg_db = conn
+        .interact(|conn| {
+            diesel::insert_into(messages::table)
+                .values(new_msg_db)
+                .returning(NewMsgDb::as_returning())
+                .get_result(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    Ok(msg_db)
+}
+
+// 获取离线消息
+pub async fn get_offline_msg(pool: &Pool, user_id: String) -> Result<Vec<MsgDb>, InfraError> {
+    let conn = pool
+        .get()
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    let msg_list = conn
+        .interact(|conn| {
+            messages::table
+                .filter(
+                    messages::friend_id.eq(user_id).and(
+                        messages::is_read
+                            .eq(false)
+                            .and(messages::delivered.eq(false)),
+                    ),
+                )
+                .select(MsgDb::as_select())
+                .get_results(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    Ok(msg_list)
+}
+
+// 消息送达
+pub async fn msg_delivered(pool: &Pool, ids: Vec<String>) -> Result<usize, InfraError> {
+    let conn = pool
+        .get()
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    let count = conn
+        .interact(|conn| {
+            diesel::update(messages::table)
+                .filter(messages::msg_id.eq_any(ids))
+                .set(messages::delivered.eq(true))
+                .execute(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    Ok(count)
+}
+
+// 消息已读
+pub async fn msg_read(pool: &Pool, ids: Vec<String>) -> Result<usize, InfraError> {
+    let conn = pool
+        .get()
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    let count = conn
+        .interact(|conn| {
+            diesel::update(messages::table)
+                .filter(messages::msg_id.eq_any(ids))
+                .set(messages::is_read.eq(true))
+                .execute(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    Ok(count)
+}
+pub async fn msg_read_single(pool: &Pool, id: String) -> Result<usize, InfraError> {
+    let conn = pool
+        .get()
+        .await
+        .map_err(|err| InfraError::InternalServerError(err.to_string()))?;
+    let count = conn
+        .interact(|conn| {
+            diesel::update(messages::table)
+                .filter(messages::msg_id.eq(id))
+                .set(messages::is_read.eq(true))
+                .execute(conn)
+        })
+        .await
+        .map_err(adapt_infra_error)?
+        .map_err(adapt_infra_error)?;
+    Ok(count)
+}
