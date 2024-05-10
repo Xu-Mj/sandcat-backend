814f7611e721a1536b2d0c2310b3bd594bb07eba
xu_mj <331538695@qq.com>
2024-05-10 21:29:13 +0800

    feat: update to partition for messages table

--- migrations/20240322014412_messages.up.sql
+++ migrations/20240322014412_messages.up.sql
@@ -1,17 +1,12 @@
--- do not use foreign key, it has performance issue
 CREATE TABLE messages
 (
     send_id      VARCHAR NOT NULL,
     receiver_id  VARCHAR NOT NULL,
     local_id     VARCHAR NOT NULL,
     server_id    VARCHAR NOT NULL,
---     todo need a timestamp type
---     create_time    BIGINT  NOT NULL,
     send_time    BIGINT  NOT NULL,
---     seq          BIGINT,
     msg_type     INT,
     content_type INT,
     content      BYTEA,
---     is_read      BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (send_id, server_id, send_time)
-) /*PARTITION BY RANGE (send_time)*/;
+) PARTITION BY RANGE (send_time);
