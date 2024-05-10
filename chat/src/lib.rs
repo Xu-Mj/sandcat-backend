924488f2823483815b9a16f454adbfadb42ae1ee
xu_mj <331538695@qq.com>
2024-05-10 16:59:41 +0800

    feat: cancel moving the seq operation to chat

--- chat/src/lib.rs
+++ chat/src/lib.rs
@@ -1,4 +1,3 @@
-use std::sync::Arc;
 use std::time::Duration;
 
 use async_trait::async_trait;
@@ -6,41 +5,26 @@ use nanoid::nanoid;
 use rdkafka::admin::{AdminClient, AdminOptions, NewTopic, TopicReplication};
 use rdkafka::client::DefaultClientContext;
 use rdkafka::error::KafkaError;
-use rdkafka::producer::{FutureProducer, FutureRecord, Producer};
+use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::ClientConfig;
 use tonic::server::NamedService;
 use tonic::transport::Server;
-use tracing::{error, info, warn};
+use tracing::{error, info};
 
 use abi::config::Config;
 use abi::errors::Error;
 use abi::message::chat_service_server::{ChatService, ChatServiceServer};
-use abi::message::db_service_client::DbServiceClient;
-use abi::message::{GroupMembersIdRequest, MsgResponse, MsgType, SendMsgRequest};
-use cache::Cache;
+use abi::message::{MsgResponse, MsgType, SendMsgRequest};
 use utils::typos::{GrpcHealthCheck, Registration};
-use utils::LbWithServiceDiscovery;
 
 pub struct ChatRpcService {
     kafka: FutureProducer,
     topic: String,
-    cache: Arc<dyn Cache>,
-    db_rpc: DbServiceClient<LbWithServiceDiscovery>,
 }
 
 impl ChatRpcService {
-    pub fn new(
-        kafka: FutureProducer,
-        topic: String,
-        cache: Arc<dyn Cache>,
-        db_rpc: DbServiceClient<LbWithServiceDiscovery>,
-    ) -> Self {
-        Self {
-            kafka,
-            topic,
-            cache,
-            db_rpc,
-        }
+    pub fn new(kafka: FutureProducer, topic: String) -> Self {
+        Self { kafka, topic }
     }
     pub async fn start(config: &Config) {
         let broker = config.kafka.hosts.join(",");
@@ -78,12 +62,7 @@ impl ChatRpcService {
             .await;
         info!("<chat> rpc service health check started");
 
-        let cache = cache::cache(config);
-        let db_rpc = Self::get_db_rpc_client(config)
-            .await
-            .expect("Db rpc client error");
-
-        let chat_rpc = Self::new(producer, config.kafka.topic.clone(), cache, db_rpc);
+        let chat_rpc = Self::new(producer, config.kafka.topic.clone());
         let service = ChatServiceServer::new(chat_rpc);
         info!(
             "<chat> rpc service started at {}",
@@ -96,17 +75,6 @@ impl ChatRpcService {
             .serve(config.rpc.chat.rpc_server_url().parse().unwrap())
             .await
             .unwrap();
-    }
-
-    async fn get_db_rpc_client(
-        config: &Config,
-    ) -> Result<DbServiceClient<LbWithServiceDiscovery>, Error> {
-        // use service register center to get ws rpc url
-        let channel =
-            utils::get_channel_with_config(config, &config.rpc.db.name, &config.rpc.db.protocol)
-                .await?;
-        let db_rpc = DbServiceClient::new(channel);
-        Ok(db_rpc)
     }
 
     async fn register_service(config: &Config) -> Result<(), Error> {
@@ -167,52 +135,6 @@ impl ChatRpcService {
             Err(err) => Err(err),
         }
     }
-
-    /// query members id from cache
-    /// if not found, query from db
-    async fn get_members_id(&self, group_id: &str) -> Result<Vec<String>, Error> {
-        match self.cache.query_group_members_id(group_id).await {
-            Ok(list) if !list.is_empty() => Ok(list),
-            Ok(_) => {
-                warn!("group members id is empty from cache");
-                // query from db
-                self.query_group_members_id_from_db(group_id).await
-            }
-            Err(err) => {
-                error!("failed to query group members id from cache: {:?}", err);
-                Err(err)
-            }
-        }
-    }
-
-    /// query members id from database
-    /// and set it to cache
-    async fn query_group_members_id_from_db(&self, group_id: &str) -> Result<Vec<String>, Error> {
-        let request = GroupMembersIdRequest {
-            group_id: group_id.to_string(),
-        };
-        let mut db_rpc = self.db_rpc.clone();
-        match db_rpc.group_members_id(request).await {
-            Ok(resp) => {
-                let members_id = resp.into_inner().members_id;
-
-                // save it to cache
-                if let Err(e) = self
-                    .cache
-                    .save_group_members_id(group_id, members_id.clone())
-                    .await
-                {
-                    error!("failed to save group members id to cache: {:?}", e);
-                }
-
-                Ok(members_id)
-            }
-            Err(e) => {
-                error!("failed to query group members id from db: {:?}", e);
-                Err(Error::InternalServer(e.to_string()))
-            }
-        }
-    }
 }
 
 #[async_trait]
@@ -240,51 +162,10 @@ impl ChatService for ChatRpcService {
             .and_utc()
             .timestamp_millis();
 
-        let msg_type = MsgType::try_from(msg.msg_type)
-            .map_err(|_| tonic::Status::invalid_argument("msg type is invalid"))?;
-
         // send msg to kafka
         let payload = serde_json::to_string(&msg).unwrap();
         // let kafka generate key, then we need set FutureRecord<String, type>
         let record: FutureRecord<String, String> = FutureRecord::to(&self.topic).payload(&payload);
-        self.kafka
-            .begin_transaction()
-            .map_err(|_| tonic::Status::internal("begin kafka transaction error"))?;
-
-        // increase the user sequence
-        match msg_type {
-            MsgType::FriendDelete
-            | MsgType::FriendApplyReq
-            | MsgType::FriendApplyResp
-            | MsgType::SingleMsg
-            | MsgType::SingleCallInviteNotAnswer
-            | MsgType::SingleCallInviteCancel
-            | MsgType::Hangup
-            | MsgType::ConnectSingleCall
-            | MsgType::RejectSingleCall => {
-                let seq = self.cache.increase_seq(&msg.receiver_id).await?;
-                msg.seq = seq;
-            }
-            MsgType::GroupMsg
-            | MsgType::GroupInvitation
-            | MsgType::GroupInviteNew
-            | MsgType::GroupMemberExit
-            | MsgType::GroupDismiss
-            | MsgType::GroupDismissOrExitReceived
-            | MsgType::GroupInvitationReceived
-            | MsgType::GroupUpdate => {
-                // group operation, update the cache in the db service
-                // query group members id from the cache
-                let mut members = self.get_members_id(&msg.receiver_id).await?;
-
-                // retain the members id
-                members.retain(|id| id != &msg.send_id);
-
-                // increase the members seq
-                self.cache.incr_group_seq(&members).await?;
-            }
-            _ => {}
-        }
 
         let err = match self.kafka.send(record, Duration::from_secs(0)).await {
             Ok(_) => String::new(),
